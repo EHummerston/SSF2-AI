827bacfd0a203fa919cc648746cdb01e875f89d4
EHummerston <magiktrev3@gmail.com>
2015-11-16 09:35:27 +1100

    commenting my shitty code
    Signed-off-by: EHummerston <magiktrev3@gmail.com>

--- main.lua
+++ main.lua
@@ -1,13 +1,19 @@
 require("bert")
+-- add 'require' bits here
 pad = {}
 botData = {}
 botData[1] = {}
 botData[2] = {}
 
+-- TODO
+--	*allow "Forward", "Back" strings
+--	*maybe "LP","MP" etc?
 function setButton(playerSlot,button,state)
 	pad[string.format("P%i %s",playerSlot,button)] = state
 end
 
+-- returns true if player #(playerSlot) is facing right use 'getDirectionButton
+--	(playerSlot) for inputs, this function is not often relevant
 function isFacingRight (playerSlot)
 	if playerSlot % 2 == 1 then	--playerSlot is odd (1)
 		return memory.read_u8(0x5F3) == 0x40
@@ -16,10 +22,9 @@ function isFacingRight (playerSlot)
 	end
 end
 
--- I don't remember how this function works,
---	but give it will return "Right" or "Left"
---	where 'forward' is a boolean of whether you want
---	what is forward (true) or backward (false)
+-- I don't remember how this function works, but it will return "Right" or
+--	"Left" where 'forward' is a boolean of whether you want what is forward
+--	(true) or backward (false)
 function getDirectionButton (playerSlot, forward)
 	intendedRight = forward
 	if not isFacingRight(playerSlot) then
@@ -33,6 +38,8 @@ function getDirectionButton (playerSlot, forward)
 	end	
 end
 
+-- Returns true if given player is crouching
+--	Includes crouching attacks and special moves (e.g. low tiger shot)
 function isCrouching (playerSlot)
 	if playerSlot % 2 == 1 then
 		return memory.read_u8(0x544) == 0x1
@@ -41,6 +48,8 @@ function isCrouching (playerSlot)
 	end
 end
 
+-- returns true if given player is in the air
+--	includes not only jumps, but special moves that make a character airborne
 function isAir (playerSlot)
 	if playerSlot % 2 == 1 then
 		return memory.read_u8(0x548) == 0x1
@@ -49,6 +58,8 @@ function isAir (playerSlot)
 	end
 end
 
+-- returns the distance from opponent stored for given player
+--	is this the same value for both players?? probably
 function getDistance (playerSlot)
 	if playerSlot % 2 == 1 then
 		return memory.read_u8(0x5EB)
@@ -57,6 +68,10 @@ function getDistance (playerSlot)
 	end
 end
 
+-- returns true if given player is performing an attack
+--	includes the startup, active and recovery frames of all (afaik) attacks
+--	does not include whether or not a fireball is active, only whether they are
+--	in a fireball animation
 function isAttacking (playerSlot)
 	if playerSlot % 2 == 1 then
 		return memory.read_u8(0x5E9) == 0x1
@@ -65,6 +80,8 @@ function isAttacking (playerSlot)
 	end
 end
 
+-- returns true if given player has a projectile active
+--	(they can't make another)
 function hasFireball (playerSlot)
 	if playerSlot % 2 == 1 then
 		return memory.read_u8(0x5F8) == 0x1
@@ -73,20 +90,22 @@ function hasFireball (playerSlot)
 	end
 end
 
+-- BEGIN
+--	THE REAL SHIT
+
 bertInit(1)
 bertInit(2)
 
 while true do
-	pad = {}
+
+	pad = {}	-- input is set to nil every loop
 
 	bertAdvance(1)
 	bertAdvance(2)
 	
 	joypad.set(pad)
-	
 	emu.frameadvance()
 	
-	
-	
-
 end
+
+-- DO NOT REACH HERE
