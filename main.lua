123c4b147599c4ec619381a9ea7d158f2fa4f490
EHummerston <ed.hummerston@gmail.com>
2016-08-14 16:03:04 +1000

    fix merge artifacts, did not catch them originally
    Signed-off-by: EHummerston <ed.hummerston@gmail.com>

--- main.lua
+++ main.lua
@@ -1,25 +1,9 @@
+
 require("Bert")
 -- add 'require' bits here
 
-<<<<<<< HEAD
-function setButton(playerSlot,button,state)
-	pad[string.format("P%i %s",playerSlot,button)] = state
-end
-
--- returns true if player #(playerSlot) is facing right
---	use 'getDirectionButton (playerSlot) for inputs,
---	this function is not often relevant
-function isFacingRight (playerSlot)
-	if playerSlot % 2 == 1 then	--playerSlot is odd (1)
-		return memory.read_u8(0x5F3) == 0x40
-	else	--playerSlot is even (2)
-		return memory.read_u8(0x833) == 0x40
-	end
-end
-=======
 bertOne = Bert:create(1)
 bertTwo = Bert:create(2)
->>>>>>> readme
 
 bertTwo.playerSlot = 2 -- playerSlot not initialising properly in constructor.
 
@@ -38,24 +22,6 @@ while true do
 	end
 	joypad.set(pads)
 
-<<<<<<< HEAD
--- script begins here
-
-bertInit(1)
-bertInit(2)
-
--- main loop
-
-while true do
-
-	pad = {}	-- input is set to nil every loop
-
-	bertAdvance(1)
-	bertAdvance(2)
-	
-	joypad.set(pad)
-=======
->>>>>>> readme
 	emu.frameadvance()
 	
 end
