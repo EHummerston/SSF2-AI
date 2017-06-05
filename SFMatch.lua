8ca0d3ef24aca56200e4934d14ee8bbc535115a8
Edward Hummerston <ed.hummerston@gmail.com>
2017-06-06 08:24:09 +1000

    SFMatch object
    SFMatch is now an object that is passed two Bot objects and manages
    them, this removes the Bert and Zemgief dependencies in this file.

--- SFMatch.lua
+++ SFMatch.lua
@@ -6,95 +6,91 @@
 -- 2017 Edward Hummerston
 -----------------------------------------------------------------------------
 
-require("Bert")
-require("Zemgief")
 require("SFDraw")
+
+SFMatch = {}
 
 debugUI = true -- console outputs and extra text within emulator space
 
-botOne = nil
-botTwo = nil
+function SFMatch.run (botOne, botTwo)
+   while true do  -- loop once per frame
 
-botOne = Bert.new(1)
-botTwo = Bert.new(2)
-
-while true do  -- loop once per frame
-
-   -- erase input values from previous frame
-   if(botOne ~= nil) then
-      botOne:resetPad()
-   end
-   if(botTwo ~= nil) then
-      botTwo:resetPad()
-   end
-   
-   -- this memory value is currently incorrect.
-   if memory.read_u8(0x10083) == 0x0 then -- if the game is in a fight state
-      -- algorithms resolve their controller states
+      -- erase input values from previous frame
       if(botOne ~= nil) then
-         botOne:advance()
+         botOne:resetPad()
       end
       if(botTwo ~= nil) then
-         botTwo:advance()
+         botTwo:resetPad()
       end
-   end
-   
-   -- convert separate player input tables to Bizhawk table
-   pads = {}
-   if(botOne ~= nil) then
-      pads = botOne:getPad()
-      if(botTwo ~= nil) then
-         botTwoPad = botTwo:getPad()
-
-         -- append first table with the second, formatting is already correct
-         for k,v in pairs(botTwoPad) do
-            pads[k] = v
+      
+      -- this memory value is currently incorrect.
+      if memory.read_u8(0x10083) == 0x0 then -- if the game is in a fight state
+         -- algorithms resolve their controller states
+         if(botOne ~= nil) then
+            botOne:advance()
+         end
+         if(botTwo ~= nil) then
+            botTwo:advance()
          end
       end
-   elseif(botTwo ~= nil) then
-      pads = botTwo:getPad()
-   end
-   
-   -- send to emulator
-   joypad.set(pads)
-
-   -- p1 info
-   if(botOne ~= nil) then
-      SFDraw.drawName(1, botOne:getName())
-      SFDraw.drawPad(1)
-   end
-   
-   -- p2 info
-   if(botTwo ~= nil) then
-      SFDraw.drawName(2, botTwo:getName())
-      SFDraw.drawPad(2)
-   end
-   
-   if(debugUI) then
-      -- proximity block boolean
-      gui.pixelText(4,0,"atk " .. tostring(memory.read_u8(0x5E9) == 0x1))
-      gui.pixelText(148,0,"atk " .. tostring(memory.read_u8(0x829) == 0x1))
       
-      
-      -- action print
+      -- convert separate player input tables to Bizhawk table
+      pads = {}
       if(botOne ~= nil) then
-         gui.pixelText(4,8,botOne:getAction())
-      end
-      if(botTwo ~= nil) then
-         gui.pixelText(148,8,botTwo:getAction())
-      end
-      
-      gui.pixelText(122,23,tostring(memory.read_u8(0x5EB))) -- distance
-      
-      gui.pixelText(16,35,tostring(memory.read_u8(0x530)))  -- p1 health
-      gui.pixelText(227,35,tostring(memory.read_u8(0x770))) -- p2 health
-   
-   end
-   
-   -- emulator resolves cycle
-   emu.frameadvance()
-   
-end
+         pads = botOne:getPad()
+         if(botTwo ~= nil) then
+            botTwoPad = botTwo:getPad()
 
--- DO NOT REACH HERE
-error("Infinite loop broken")
+            -- append first table with the second, formatting is already correct
+            for k,v in pairs(botTwoPad) do
+               pads[k] = v
+            end
+         end
+      elseif(botTwo ~= nil) then
+         pads = botTwo:getPad()
+      end
+      
+      -- send to emulator
+      joypad.set(pads)
+
+      -- p1 info
+      if(botOne ~= nil) then
+         SFDraw.drawName(1, botOne:getName())
+         SFDraw.drawPad(1)
+      end
+      
+      -- p2 info
+      if(botTwo ~= nil) then
+         SFDraw.drawName(2, botTwo:getName())
+         SFDraw.drawPad(2)
+      end
+      
+      if(debugUI) then
+         -- proximity block boolean
+         gui.pixelText(4,0,"atk " .. tostring(memory.read_u8(0x5E9) == 0x1))
+         gui.pixelText(148,0,"atk " .. tostring(memory.read_u8(0x829) == 0x1))
+         
+         
+         -- action print
+         if(botOne ~= nil) then
+            gui.pixelText(4,8,botOne:getAction())
+         end
+         if(botTwo ~= nil) then
+            gui.pixelText(148,8,botTwo:getAction())
+         end
+         
+         gui.pixelText(122,23,tostring(memory.read_u8(0x5EB))) -- distance
+         
+         gui.pixelText(16,35,tostring(memory.read_u8(0x530)))  -- p1 health
+         gui.pixelText(227,35,tostring(memory.read_u8(0x770))) -- p2 health
+      
+      end
+      
+      -- emulator resolves cycle
+      emu.frameadvance()
+      
+   end
+
+   -- DO NOT REACH HERE
+   error("Infinite loop broken")
+end
